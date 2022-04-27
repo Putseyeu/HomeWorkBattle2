1d226d5193b9e3a0300822e77fd0a37e7b0103e5
Putseyeu <putseyeu.a.d@gmail.com>
2022-04-27 20:58:19 +0200

    4.27.2022

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkGladiator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Battle
+    {
+
+
+        public Battle()
+        {
+
+        }
+
+        public 
+    }
+
+    class Magician
+    {
+        private int _health = 100;
+        
+        public string Name { get; private set; }
+        public int Health => _health;
+
+        public Magician()
+        {
+            SetName();
+        }
+
+        public void SetName()
+        {
+            Console.WriteLine("Вы представитель магической гильдии! Назовите своё имя!");
+            Name = Console.ReadLine();
+        }
+
+
+    }
+
+    class Warrior
+    {
+        private string _name = "";
+        private int _health = 100;
+
+        public Warrior()
+        {
+
+        }
+    }
+}
